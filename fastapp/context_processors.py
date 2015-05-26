b67cbc7328c18585ba71729a78b31c314d4a718e
Philip Sahli <philip@sahli.net>
2015-05-26 20:26:53 +0200

    add versions to context

--- fastapp/context_processors.py
+++ fastapp/context_processors.py
@@ -1,5 +1,14 @@
 from django.conf import settings
+from planet import __VERSION__ as PLANET_VERSION
+from fastapp import __version__ as FASTAPP_VERSION
 
 
 def dragon_url(request):
     return {'DRAGON_URL': settings.DRAGON_URL}
+
+
+def versions(request):
+    return {
+        'PLANET_VERSION': PLANET_VERSION,
+        'FASTAPP_VERSION': FASTAPP_VERSION,
+    }
