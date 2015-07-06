c495ac0b8bf7041fba92e28d102c6f42d7404f71
Philip Sahli <philip@sahli.net>
2015-07-06 17:00:50 +0200

    add logs for async response when RUNNING

--- fastapp/views.py
+++ fastapp/views.py
@@ -404,6 +404,11 @@ class DjendExecView(View, ResponseUnavailableViewMixing, DjendMixin):
                 data = {'status': transaction.get_status_display()}
                 redirect_to = request.get_full_path()
                 data.update({'url': redirect_to})
+                #if request.GET.get('include_logs', None):
+                data.update({'logs':
+                    json.loads(serializers.serialize("json", transaction.logs.all()))
+                })
+
         else:
             request_data = self._prepare_request(request, exec_model)
             transaction = Transaction(apy=exec_model)
