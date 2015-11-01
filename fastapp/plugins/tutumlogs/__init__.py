f4fe560914a990f9be894ef7f64e3dc33fd48537
Philip Sahli <philip@sahli.net>
2015-11-01 08:21:46 +0100

    fix plugin tutumlogs

--- fastapp/plugins/tutumlogs/__init__.py
+++ fastapp/plugins/tutumlogs/__init__.py
@@ -1,12 +1,15 @@
+import os
+import logging
+import inspect
+
+from django.conf import settings
 
 from fastapp.plugins import register_plugin, Plugin
 
-@register_plugin
-class DataStorePlugin(Plugin):
+logger = logging.getLogger(__name__)
 
-	def attach_worker(self, **kwargs):
-		logger.info("Attach to worker")
-		return PsqlDataStore(schema=kwargs['USER'], **kwargs)
+@register_plugin
+class TutumLogsPlugin(Plugin):
 
 	@classmethod
 	def init(cls):
@@ -16,34 +19,32 @@ class DataStorePlugin(Plugin):
 		settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS + (template_path,)
 
 	def config(self, base):
-		plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
-		plugin_settings['USER'] = base.name.replace("-", "_")
-		plugin_settings['PASSWORD'] = base.name
+		plugin_settings = settings.FASTAPP_PLUGINS_CONFIG[self.fullname]
 		return plugin_settings
 
 	def cockpit_context(self):
+		logs = []
+		try:
+			plugin_settings = settings.FASTAPP_PLUGINS_CONFIG[self.fullname]
 
-        try:
-    		plugin_settings = settings.FASTAPP_PLUGINS_CONFIG[self.fullname]
+			import tutum
+			#tutum.user = plugin_settings['USER']
+			#tutum.apikey = plugin_settings['API_KEY']
 
-            import tutum
-            #tutum.user = plugin_settings['USER']
-            #tutum.apikey = plugin_settings['API_KEY']
+			service_uuid = os.environ['TUTUM_SERVICE_API_URI'].split("/")[-2]
 
-            service_uuid = os.environ['TUTUM_SERVICE_API_URI'].split("/")[-2]
 
-            logs = []
+			def log_handler(message):
+				logs.append(message)
 
-            def log_handler(message):
-                logs.append(message)
-
-            container = tutum.Container.fetch(service_uuid)
-            container.logs(tail=200, follow=False, log_handler=log_handler)
-            msg = "Well done..."
-        except Exception, e:
-            msg = repr(e)
+			container = tutum.Container.fetch(service_uuid)
+			container.logs(tail=200, follow=False, log_handler=log_handler)
+			msg = "Well done..."
+		except Exception, e:
+			msg = repr(e)
+			logger.exception(e)
 
 		return {
 			'logs': logs,
-            'message': msg
+			'message': msg
 		}
