5a78d8b5e39f1d1675e47d1786a8fde13b28e3bd
Philip Sahli <philip@sahli.net>
2015-11-16 10:08:28 +0100

    add StatsPlugin

---
+++ fastapp/plugins/stats/__init__.py
@@ -0,0 +1,28 @@
+import os
+import inspect
+import logging
+
+from django.conf import settings
+
+from fastapp.plugins import register_plugin, Plugin
+from fastapp.models import Transaction
+
+logger = logging.getLogger(__name__)
+
+@register_plugin
+class StatsPlugin(Plugin):
+
+	@classmethod
+	def init(cls):
+		logger.info("Init %s" % cls)
+		plugin_path = os.path.dirname(inspect.getfile(cls))
+		template_path = os.path.join(plugin_path, "templates")
+		settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS + (template_path,)
+
+	def cockpit_context(self):
+		data={}
+		data.update({
+			'oldest_transaction': Transaction.objects.all().order_by('-created')[0].created,
+			'count_transactions': Transaction.objects.all().count()
+		})
+		return data
