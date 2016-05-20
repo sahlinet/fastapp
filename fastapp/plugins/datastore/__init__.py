e2cbea4d23136f33fe5e4f326507f6c03df71b28
Philip Sahli <philip@sahli.net>
2016-05-20 03:17:36 -0400

    introduce rollback on datastore plugin

--- fastapp/plugins/datastore/__init__.py
+++ fastapp/plugins/datastore/__init__.py
@@ -34,210 +34,216 @@ Base = declarative_base()
 
 class DataObject(Base):
 
-	__tablename__ = 'data_table'
+    __tablename__ = 'data_table'
 
-	id = Column(Integer, primary_key=True)
-	created_on = Column(DateTime, default=datetime.datetime.now)
-	data = Column(JSON)
+    id = Column(Integer, primary_key=True)
+    created_on = Column(DateTime, default=datetime.datetime.now)
+    data = Column(JSON)
 
 
 class DataStore(object):
 
-	ENGINE = 'sqlite:///:memory:'
+    ENGINE = 'sqlite:///:memory:'
 
-	def __init__(self, schema=None, password=None, *args, **kwargs):
-		if schema:
-			self.schema = schema.replace("-", "_")
-		else:
-			self.schema = schema
-		self.kwargs = kwargs
-		logger.info("Working with schema: %s" % schema)
-		logger.info("Working with config: %s" % str(kwargs))
+    def __init__(self, schema=None, password=None, *args, **kwargs):
+        if schema:
+            self.schema = schema.replace("-", "_")
+        else:
+            self.schema = schema
+        self.kwargs = kwargs
+        logger.info("Working with schema: %s" % schema)
+        logger.info("Working with config: %s" % str(kwargs))
 
-		self.password = password
+        self.password = password
 
-		# set schema for table creation
-		DataObject.__table__.schema = self.schema
+        # set schema for table creation
+        DataObject.__table__.schema = self.schema
 
-		# create session with engine
-		self.engine = create_engine(self.__class__.ENGINE % kwargs, echo=True)
-		Session = sessionmaker(bind=self.engine)
-		self.session = Session()
+        # create session with engine
+        self.engine = create_engine(self.__class__.ENGINE % kwargs, echo=True)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
 
-		# set schema for sql executions
-		self._execute("SET search_path TO %s" % self.schema)
-		self.session.commit()
+        # set schema for sql executions
+        self._execute("SET search_path TO %s" % self.schema)
+        self._commit()
 
-	def _activate_plpythonu(self):
-		"""As from link http://stackoverflow.com/questions/18209625/how-do-i-modify-fields-inside-the-new-postgresql-json-datatype"""
+    def _activate_plpythonu(self):
+        """As from link http://stackoverflow.com/questions/18209625/how-do-i-modify-fields-inside-the-new-postgresql-json-datatype"""
 
-		self._execute("CREATE EXTENSION IF NOT EXISTS plpythonu;")
-		self.session.commit()
-		self._execute("CREATE LANGUAGE plpythonu;")
-		self.session.commit()
+        self._execute("CREATE EXTENSION IF NOT EXISTS plpythonu;")
+        self._commit()
+        self._execute("CREATE LANGUAGE plpythonu;")
+        self._commit()
 
-	def _json_update(self):
+    def _json_update(self):
 
-		self._execute("""CREATE OR REPLACE FUNCTION json_update(data json, key text, value text)
-		 RETURNS json
-		 AS $$
-		    import json
-		    json_data = json.loads(data)
-		    json_data[key] = value
-		    return json.dumps(json_data, indent=4)
-		 $$ LANGUAGE plpythonu;""")
-		self.session.commit()
+        self._execute("""CREATE OR REPLACE FUNCTION json_update(data json, key text, value text)
+         RETURNS json
+         AS $$
+            import json
+            json_data = json.loads(data)
+            json_data[key] = value
+            return json.dumps(json_data, indent=4)
+         $$ LANGUAGE plpythonu;""")
+        self._commit()
 
-	def init_store(self):
-		"""
-		Runs on server with super user privileges
-		"""
-		if self.schema:
-			# User
-			try:
-				self._execute("CREATE USER %s WITH PASSWORD '%s'" % (self.schema,
-					self.password))
-			except Exception, e:
-				self.session.rollback()
-				if "already exists" in repr(e):
-					logger.info("Could not create user '%s', already exists." % self.schema)
-					logger.info("Update password anyway for user '%s'" % self.schema)
-					self._execute("ALTER USER %s WITH PASSWORD '%s'" % (self.schema, self.password))
-				else:
-					logger.error("Could not create user '%s'." % self.schema)
+    def init_store(self):
+        """
+        Runs on server with super user privileges
+        """
+        if self.schema:
+            # User
+            try:
+                self._execute("CREATE USER %s WITH PASSWORD '%s'" % (self.schema,
+                    self.password))
+            except Exception, e:
+                self.session.rollback()
+                if "already exists" in repr(e):
+                    logger.info("Could not create user '%s', already exists." % self.schema)
+                    logger.info("Update password anyway for user '%s'" % self.schema)
+                    self._execute("ALTER USER %s WITH PASSWORD '%s'" % (self.schema, self.password))
+                else:
+                    logger.error("Could not create user '%s'." % self.schema)
 
-			# Schema
-			try:
-				self.engine.execute(CreateSchema(self.schema))
-				logger.info("Schema created")
-			except Exception, e:
-				self.session.rollback()
-				if "already exists" in repr(e):
-					logger.info("Could not create schema '%s', already exists." % self.schema)
-				else:
-					logger.error("Could not create schema '%s'." % self.schema)
+            # Schema
+            try:
+                self.engine.execute(CreateSchema(self.schema))
+                logger.info("Schema created")
+            except Exception, e:
+                self.session.rollback()
+                if "already exists" in repr(e):
+                    logger.info("Could not create schema '%s', already exists." % self.schema)
+                else:
+                    logger.error("Could not create schema '%s'." % self.schema)
 
-			# Permissions
-			self._execute("GRANT USAGE ON SCHEMA %s to %s;" % (self.schema, self.schema))
-			self._execute("GRANT ALL ON ALL TABLES IN  SCHEMA %s to %s;" % (self.schema, self.schema))
-			self._execute("GRANT ALL ON ALL SEQUENCES IN  SCHEMA %s to %s;" % (self.schema, self.schema))
-			self.session.commit()
-		else:
-			logger.warn("No schema specified")
-		self._prepare()
-		return "init_store '%s' done" % self.schema
+            # Permissions
+            self._execute("GRANT USAGE ON SCHEMA %s to %s;" % (self.schema, self.schema))
+            self._execute("GRANT ALL ON ALL TABLES IN  SCHEMA %s to %s;" % (self.schema, self.schema))
+            self._execute("GRANT ALL ON ALL SEQUENCES IN  SCHEMA %s to %s;" % (self.schema, self.schema))
+            self._commit()
+        else:
+            logger.warn("No schema specified")
+        self._prepare()
+        return "init_store '%s' done" % self.schema
 
-	def _prepare(self):
-		Base.metadata.create_all(self.engine)
-		self.session.commit()
+    def _prepare(self):
+        Base.metadata.create_all(self.engine)
+        self._commit()
 
-	def write_obj(self, obj):
-		self.session.add(obj)
-		self.session.commit()
+    def write_obj(self, obj):
+        self.session.add(obj)
+        self._commit()
 
-	def write_dict(self, data_dict):
-		obj_dict = DataObject(data=data_dict)
-		return self.write_obj(obj_dict)
+    def write_dict(self, data_dict):
+        obj_dict = DataObject(data=data_dict)
+        return self.write_obj(obj_dict)
 
-	def update(self, obj):
-		flag_modified(obj, "data")
-		self.session.commit()
-		
+    def _commit(self):
+        try:
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
 
-	def all(self):
-		return self.session.query(DataObject).all()
+    def update(self, obj):
+        flag_modified(obj, "data")
+        self._commit()
 
-	def count(self):
-		return self.session.query(DataObject).count()
+    def all(self):
+        return self.session.query(DataObject).all()
 
-	def filter(self, k, v):
-		return self.session.query(DataObject).filter(text("data->>'"+k+"' = '"+v+"';")).all()
+    def count(self):
+        return self.session.query(DataObject).count()
 
-	def delete(self, obj):
-		self.session.delete(obj)
-		return self.session.commit()
+    def filter(self, k, v):
+        return self.session.query(DataObject).filter(text("data->>'"+k+"' = '"+v+"';")).all()
 
-	def save(self, obj):
-		return self.session.commit()
+    def delete(self, obj):
+        self.session.delete(obj)
+        return self._commit()
 
-	def get(self, k, v):
-		result = self.filter(k, v)
-		if len(result) > 1:
-			raise Exception("More than one row returned! (%s)" % len(result))
-		elif len(result) < 1:
-			return None
-		return result[0]
+    def save(self, obj):
+        return self._commit()
 
-	def _execute(self, sql, result=None):
-		logger.info(sql)
-		result = self.session.execute(sql)
-		self.session.commit()
-		return result
+    def get(self, k, v):
+        result = self.filter(k, v)
+        if len(result) > 1:
+            raise Exception("More than one row returned! (%s)" % len(result))
+        elif len(result) < 1:
+            return None
+        return result[0]
 
-	def truncate(self):
-		self._execute("TRUNCATE %s.data_table" % self.schema)
+    def _execute(self, sql, result=None):
+        logger.info(sql)
+        result = self.session.execute(sql)
+        self._commit()
+        return result
+
+    def truncate(self):
+        self._execute("TRUNCATE %s.data_table" % self.schema)
 
 
 def resultproxy_to_list(proxy):
-	l = []
-	for row in proxy:
-		l.append(row.__dict__)
-	return l
+    l = []
+    for row in proxy:
+        l.append(row.__dict__)
+    return l
 
 
 class PsqlDataStore(DataStore):
-	__metaclass__ = Singleton
+    __metaclass__ = Singleton
 
-	ENGINE = 'postgresql+psycopg2://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s'
+    ENGINE = 'postgresql+psycopg2://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s'
 
-	def query(self, k, v):
-		q = """SELECT id, json_string(data,'%s'
-				FROM things
-				WHERE json_string(data,'%s')
-				LIKE '%s%';""" % (k, v)
+    def query(self, k, v):
+        q = """SELECT id, json_string(data,'%s'
+                FROM things
+                WHERE json_string(data,'%s')
+                LIKE '%s%';""" % (k, v)
 
 
 class PsqlDataStoreSingleton(PsqlDataStore):
-	__metaclass__ = Singleton
+    __metaclass__ = Singleton
 
 
 @register_plugin
 class DataStorePlugin(Plugin):
 
-	def attach_worker(self, **kwargs):
-		logger.info("Attach to worker")
-		return PsqlDataStoreSingleton(schema=kwargs['USER'],
-		 							  password=kwargs['PASSWORD'], **kwargs)
+    def attach_worker(self, **kwargs):
+        logger.info("Attach to worker")
+        return PsqlDataStoreSingleton(schema=kwargs['USER'],
+                                      password=kwargs['PASSWORD'], **kwargs)
 
-	@classmethod
-	def init(cls):
-		logger.info("Init %s" % cls)
-		plugin_path = os.path.dirname(inspect.getfile(cls))
-		template_path = os.path.join(plugin_path, "templates")
-		settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS + (template_path,)
+    @classmethod
+    def init(cls):
+        logger.info("Init %s" % cls)
+        plugin_path = os.path.dirname(inspect.getfile(cls))
+        template_path = os.path.join(plugin_path, "templates")
+        settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS + (template_path,)
 
-	def config(self, base):
-		plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
-		plugin_settings['USER'] = base.name.replace("-", "_")
-		plugin_settings['PASSWORD'] = base.executor.password
-		return plugin_settings
+    def config(self, base):
+        plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
+        plugin_settings['USER'] = base.name.replace("-", "_")
+        plugin_settings['PASSWORD'] = base.executor.password
+        return plugin_settings
 
-	def on_start_base(self, base):
-		plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
-		store = PsqlDataStore(schema=base.name, password=base.executor.password,
-							  keep=False, **plugin_settings)
-		return store.init_store()
+    def on_start_base(self, base):
+        plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
+        store = PsqlDataStore(schema=base.name, password=base.executor.password,
+                              keep=False, **plugin_settings)
+        return store.init_store()
 
-	def cockpit_context(self):
-		plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
-		SCHEMAS = "SELECT schema_name FROM information_schema.schemata;"
-		TABLESPACES = """SELECT array_to_json(array_agg(row_to_json(t))) FROM (
-				SELECT *, pg_tablespace_size(spcname) FROM pg_tablespace
-			) t;"""
-		CONNECTIONS = "SELECT * FROM pg_stat_activity;"
+    def cockpit_context(self):
+        plugin_settings = settings.FASTAPP_PLUGINS_CONFIG['fastapp.plugins.datastore']
+        SCHEMAS = "SELECT schema_name FROM information_schema.schemata;"
+        TABLESPACES = """SELECT array_to_json(array_agg(row_to_json(t))) FROM (
+                SELECT *, pg_tablespace_size(spcname) FROM pg_tablespace
+            ) t;"""
+        CONNECTIONS = "SELECT * FROM pg_stat_activity;"
 
-		return {
-			'SCHEMAS': [row for row in PsqlDataStore(keep=False, **plugin_settings)._execute(SCHEMAS)],
-			'TABLESPACES': [row for row in PsqlDataStore(keep=False, **plugin_settings)._execute(TABLESPACES)][0],
-			'CONNECTIONS': [row for row in PsqlDataStore(keep=False, **plugin_settings)._execute(CONNECTIONS)],
-		}
+        return {
+            'SCHEMAS': [row for row in PsqlDataStore(keep=False, **plugin_settings)._execute(SCHEMAS)],
+            'TABLESPACES': [row for row in PsqlDataStore(keep=False, **plugin_settings)._execute(TABLESPACES)][0],
+            'CONNECTIONS': [row for row in PsqlDataStore(keep=False, **plugin_settings)._execute(CONNECTIONS)],
+        }
